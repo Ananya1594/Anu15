13735aaff497cf892eb6d705ef7980b88fd281ad
Ananya1594 <ananyaganaps@gmail.com>
2016-10-07 10:40:51 +0530

    Add files via upload

---
+++ Loginbean.java
@@ -0,0 +1,25 @@
+package com.store.model;
+
+public class Loginbean 
+{
+	private String uname;
+	private String pass;
+	
+	public String getUsername(){
+		return uname;
+	}
+	
+	public void setUsername(String uname){
+		this.uname = uname;
+	}
+	
+	public String getpassword(){
+		return pass;
+	}
+	
+	public void setpassword(String pass){
+		this.pass = pass;
+	}
+	
+}
+
